1682cd797a17c733197fd2a818f21598a2f1a037
shinnaga <shinnaga.dev@gmail.com>
2020-02-04 17:15:28 +0900

    message change

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
-  const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+  const target = process.env.TARGET || 'Hello!';
+  res.send(`aaa ${target}!`);
 });
 
 const port = process.env.PORT || 8080;
